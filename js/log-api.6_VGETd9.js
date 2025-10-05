72e3714e949483ba4b7cbeda3137557e4c65099a
theaarch <theaarch@users.noreply.github.com>
2025-10-05 18:04:01 +0000

    deploy: 6ef46348f3e8be8617f5c2b872f5e8d19419cbbd

---
+++ js/log-api.6_VGETd9.js
@@ -0,0 +1,1 @@
+import{bh as t}from"./index.BB2sVePw.js";const e="/api/v1/logs",s={getPage:s=>t({url:`${e}/page`,method:"get",params:s}),getVisitTrend:s=>t({url:`${e}/visit-trend`,method:"get",params:s}),getVisitStats:()=>t({url:`${e}/visit-stats`,method:"get"})};export{s as L};

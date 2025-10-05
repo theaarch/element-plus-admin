f354c265e9521940f1da48d35186f44b3753dcd1
theaarch <theaarch64@gmail.com>
2025-10-06 02:15:07 +0800

    Update index.vue

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -38,7 +38,6 @@
       <!-- 登录页底部版权 -->
       <el-text size="small" class="py-2.5! fixed bottom-0 text-center">
         Copyright © 2021 - 2025 youlai.tech All Rights Reserved.
-        <a href="http://beian.miit.gov.cn/" target="_blank">皖ICP备20006496号-2</a>
       </el-text>
     </div>
   </div>
